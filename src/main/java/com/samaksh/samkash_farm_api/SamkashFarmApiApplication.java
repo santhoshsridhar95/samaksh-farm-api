c63de8ca1a39a19ef04487c7cb7c7e526e12fb12
santhoshsridhar95 <47470408+santhoshsridhar95@users.noreply.github.com>
2026-06-06 03:46:32 +0530

    1st initial setup commit

---
+++ src/main/java/com/samaksh/samkash_farm_api/SamkashFarmApiApplication.java
@@ -0,0 +1,13 @@
+package com.samaksh.samkash_farm_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamkashFarmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamkashFarmApiApplication.class, args);
+	}
+
+}
